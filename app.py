2577189c34f5027d46fad63e8e643e7145123bea
Young-Joon Park <yopark@sas.upenn.edu>
2023-11-24 02:41:27 -0800

    Wrap project with docker

--- app.py
+++ app.py
@@ -4,6 +4,5 @@ from website import create_app
 
 app = create_app()
 
-if __name__ == '__main__':
-    app.run(port=5001, debug=True)
-
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
